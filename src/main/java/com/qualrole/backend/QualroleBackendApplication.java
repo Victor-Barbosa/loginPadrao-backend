416f390b2388ffb945952c4e45f2038a44c0ad3f
Victor-Barbosa <victorbarbosadev@gmail.com>
2025-03-21 12:07:23 -0300

    Commit inicial

---
+++ src/main/java/com/qualrole/backend/QualroleBackendApplication.java
@@ -0,0 +1,13 @@
+package com.qualrole.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QualroleBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QualroleBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qualrole/backend/QualroleBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qualrole.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QualroleBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
