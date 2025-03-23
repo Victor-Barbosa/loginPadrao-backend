512d0402d1e41cc92b20141494652fbf2f579990
Victor-Barbosa <victorbarbosadev@gmail.com>
2025-03-23 18:35:37 -0300

    service para usuario completo

---
+++ src/main/java/com/qualrole/backend/user/service/CompleteSystemUserService.java
@@ -0,0 +1,43 @@
+package com.qualrole.backend.user.service;
+
+import com.qualrole.backend.user.builder.UserBuilder;
+import com.qualrole.backend.user.dto.CompleteSystemUserDTO;
+import com.qualrole.backend.user.entity.SystemUser;
+import com.qualrole.backend.user.repository.SystemUserRepository;
+import com.qualrole.backend.user.validation.PasswordValidator;
+import com.qualrole.backend.user.validation.UserValidator;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+/**
+ * Serviço responsável por gerenciar o registro de usuários completos no sistema.
+ */
+@Service
+public class CompleteSystemUserService {
+
+    private final SystemUserRepository systemUserRepository;
+    private final UserValidator userValidator;
+    private final PasswordValidator passwordValidator;
+    private final UserBuilder userBuilder;
+    private final PasswordEncoder passwordEncoder;
+
+    public CompleteSystemUserService(SystemUserRepository systemUserRepository, UserValidator userValidator,
+                                     PasswordValidator passwordValidator, UserBuilder userBuilder,
+                                     PasswordEncoder passwordEncoder) {
+        this.systemUserRepository = systemUserRepository;
+        this.userValidator = userValidator;
+        this.passwordValidator = passwordValidator;
+        this.userBuilder = userBuilder;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public void registerCompleteSystemUser(CompleteSystemUserDTO completeSystemUserDTO) {
+        userValidator.validateCpfCnpjUniqueness(completeSystemUserDTO.cpfCnpj(), null);
+        userValidator.validateEmailUniqueness(completeSystemUserDTO.email(), null);
+        passwordValidator.validatePassword(completeSystemUserDTO.password());
+
+        SystemUser user = userBuilder.buildNewCompleteSystemUser(completeSystemUserDTO);
+        user.setPassword(passwordEncoder.encode(completeSystemUserDTO.password()));
+        systemUserRepository.save(user);
+    }
+}
